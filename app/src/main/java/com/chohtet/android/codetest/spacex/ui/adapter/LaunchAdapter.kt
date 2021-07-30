abc601fe430e0b76738654546771a6343dd11593
ChoHtet <chocho.htet@haulio.io>
2021-07-30 21:49:34 +0630

    add name prefix

--- app/src/main/java/com/chohtet/android/codetest/spacex/ui/adapter/LaunchAdapter.kt
+++ app/src/main/java/com/chohtet/android/codetest/spacex/ui/adapter/LaunchAdapter.kt
@@ -1,10 +1,17 @@
 package com.chohtet.android.codetest.spacex.ui.adapter
 
+import android.graphics.Color
+import android.os.Build
+import android.text.Html
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.text.HtmlCompat
 import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -51,11 +58,13 @@ class LaunchAdapter(private val listener: ClickListener) : ListAdapter<
 @BindingAdapter("imageUrl")
 fun setImageUrl(imageView: AppCompatImageView, shipUrl: List<GetLaunchListQuery.Ship>) {
     if (!shipUrl.isNullOrEmpty() && !shipUrl[0].image.isNullOrEmpty()) {
+        imageView.scaleType = ImageView.ScaleType.CENTER_CROP
         imageView.load(shipUrl[0].image) {
-            crossfade(true)
+            crossfade(false)
             placeholder(R.drawable.ic_space)
         }
     } else {
+        imageView.scaleType = ImageView.ScaleType.FIT_CENTER
         imageView.load(R.drawable.ic_space) {
             crossfade(true)
         }
@@ -66,4 +75,23 @@ fun setImageUrl(imageView: AppCompatImageView, shipUrl: List<GetLaunchListQuery.
 @BindingAdapter("visibleGone")
 fun showHide(view: View, show: Boolean) {
     view.visibility = if (show) View.VISIBLE else View.GONE
+}
+
+@BindingAdapter("hyperLinkText")
+fun setHyperLink(textView: AppCompatTextView, url: String?) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        textView.text = if (!url.isNullOrEmpty()) {
+            textView.movementMethod = LinkMovementMethod.getInstance()
+            textView.setLinkTextColor(Color.BLUE)
+            Html.fromHtml(url, HtmlCompat.FROM_HTML_MODE_COMPACT)
+        } else ""
+    } else {
+        textView.text = if (!url.isNullOrEmpty()) {
+            textView.movementMethod = LinkMovementMethod.getInstance()
+            textView.setLinkTextColor(Color.BLUE)
+            Html.fromHtml(url)
+        } else ""
+    }
+
+
 }
